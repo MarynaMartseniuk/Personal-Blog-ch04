2672065cac97e3f5737e4a8d41088a0b510fa0cb
MarcM <marina.ved80gmail.com>
2024-07-04 16:26:38 -0600

    fix light/dark mode

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,28 +2,19 @@
 
 const LDmodeSwitch = document.querySelector('#LDmode');
 const container = document.querySelector('#main');
-const topFormContainer = document.querySelector('#topContainer');
-const leftFormContainer = document.querySelector('#left-appTitle');
-const rightFormContainer = document.querySelector('#right-form');
 const pageColor = document.querySelector('#pgBody');
 let mode = 'ligth';
 
 LDmodeSwitch.addEventListener('click', function () {
   if (mode === 'light') {
     mode = 'dark';
+    pageColor.setAttribute('class', 'dark');
     container.setAttribute('class', 'dark');
     LDmodeSwitch.setAttribute('class', 'dark');
-    topFormContainer.setAttribute('class', 'dark');
-    leftFormContainer.setAttribute('class', 'dark');
-    rightFormContainer.setAttribute('class', 'dark');
-    pageColor.setAttribute('class', 'dark');
   } else {
     mode = 'light';
+    pageColor.setAttribute('class', 'light');
     container.setAttribute('class', 'light');
     LDmodeSwitch.setAttribute('class', 'light');
-    topFormContainer.setAttribute('class', 'light');
-    leftFormContainer.setAttribute('class', 'light');
-    rightFormContainer.setAttribute('class', 'light');
-    pageColor.setAttribute('class', 'light');
   };
 });
